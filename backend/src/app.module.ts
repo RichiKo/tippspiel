ef4ea658e17af0d053c1c575aab766dcf7449eac
Richard Kolyadko <richard.kolyadko@mercator-leasing.de>
2025-02-18 13:48:53 +0100

    start user registration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '@app/app.controller';
+import { AppService } from '@app/app.service';
+import { AuthModule } from '@app/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormConfig from '@app/ormconfig';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(ormConfig), AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,14 @@
+import { config } from 'dotenv';
+
+if (process.env.NODE_ENV !== 'production') {
+  config();
+}
+
+if (process.env.NODE_ENV === 'production') {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  require('module-alias/register'); // Nur in Produktion laden
+}
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,4 +16,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
